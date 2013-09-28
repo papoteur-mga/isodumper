3b93770c792766b3c06b279254d6632f6024152d
SARL ENR-68 <david@david.david>
2013-09-28 15:14:54 +0200

    Update target dir "/po" to "/share/locale"

--- tools/po-compile.sh
+++ tools/po-compile.sh
@@ -8,7 +8,7 @@ find "$PWD/po" -type f -name '*.po' | \
 while read po_file; do
 	language="$(basename "$po_file")"
 	language="${language%.po}"
-	target="$PWD/po/$language/LC_MESSAGES/isodumper.mo"
+	target="$PWD/share/locale/$language/LC_MESSAGES/isodumper.mo"
 	/bin/mkdir --parents "$(dirname "$target")"
 	/usr/bin/msgfmt \
 		--check \
