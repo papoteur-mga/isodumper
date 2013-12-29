ad760639ec835765d16f3a0d18d25a8ba74f9621
SARL ENR-68 <david@david.david>
2013-12-29 15:27:16 +0100

    po formatting updates with sh po-update.sh script
    -for added a tooltip on backup "GtkButton" id="backup_select"

--- lib/isodumper.py
+++ lib/isodumper.py
@@ -6,6 +6,7 @@
 # 
 #  Modifications 2013 from papoteur <papoteur@mageialinux-online.org>
 #  and Geiger David <david.david@mageialinux-online.org>
+#
 #  This program is free software; you can redistribute it and/or 
 #  modify it under the terms of the GNU General Public License as 
 #  published by the Free Software Foundation; either version 2 of the
